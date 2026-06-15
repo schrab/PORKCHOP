6ed39603df38883d48a64d89a84c273019a818e4
schrab <schrabikus@gmail.com>
2026-06-16 03:39:32 +0700

    modem: trim Phase 1 silence 500→200ms to fit boot sound under 3s

--- src/build_info.h
+++ src/build_info.h
@@ -1,5 +1,5 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2026-06-16T03:32:24.594371"
+#define BUILD_TIME "2026-06-16T03:39:23.183675"
 #define BUILD_VERSION "0.1.1"
-#define BUILD_COMMIT "13ecc78"
+#define BUILD_COMMIT "c3dd725"
