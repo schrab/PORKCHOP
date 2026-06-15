5dfa04259cca4a02e2bc54f53041d9228c0ca579
schrab <schrabikus@gmail.com>
2026-06-16 03:17:30 +0700

    trim BOOT modem sequence to ~2s; remove long 2100Hz + 80-step burst

--- src/build_info.h
+++ src/build_info.h
@@ -1,5 +1,5 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2026-06-16T03:06:43.906535"
+#define BUILD_TIME "2026-06-16T03:17:22.109368"
 #define BUILD_VERSION "0.1.1"
-#define BUILD_COMMIT "47756d6"
+#define BUILD_COMMIT "186a2a4"
