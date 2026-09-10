11cb6117f6abece872603cf2dc936215be6adc74
schrab <schrabikus@gmail.com>
2026-09-10 18:29:05 +0700

    chore: update build_info.h for ee87ee6

--- src/build_info.h
+++ src/build_info.h
@@ -1,5 +1,5 @@
 // Auto-generated build info
 #pragma once
-#define BUILD_TIME "2026-06-18T15:45:50.080650"
+#define BUILD_TIME "2026-09-10T18:28:46.952475"
 #define BUILD_VERSION "0.1.1"
-#define BUILD_COMMIT "1d0c624"
+#define BUILD_COMMIT "ee87ee6"
